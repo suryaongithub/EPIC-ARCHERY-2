d3f4abb3dd1e29bdb79a5636ead731dc5ecf65d3
surya akshaj <96222785+suryaongithub@users.noreply.github.com>
2022-03-08 10:07:18 +0530

    Update PlayerArrow.js

--- js/PlayerArrow.js
+++ js/PlayerArrow.js
@@ -7,10 +7,10 @@ class PlayerArrow {
     this.width = width;
     this.height = height;
 
-    this.body = Bodies.rectangle(x, y, this.width, this.height);
+//     this.body = Bodies.rectangle(x, y, this.width, this.height);
     // this.body = Body.rectangle(x, y, this.width, this.height, options);
     // this.body = Bodies.rectangle(x, y, options);
-    // this.body = Bodies.rectangle(x, y, this.width, this.height, options);
+    this.body = Bodies.rectangle(x, y, this.width, this.height, options);
     
     this.image = loadImage("./assets/arrow.png");
     World.add(world, this.body);
